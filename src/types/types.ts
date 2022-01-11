c5c7d7fa6d9819f251f9e01c517fbae44027994c
kotahorii <khorii.micron@gmail.com>
2022-01-12 00:23:43 +0900

    initial commit

---
+++ src/types/types.ts
@@ -0,0 +1,12 @@
+export type Task = {
+  id: string
+  title: string
+  description: string
+}
+export type UserInfo = {
+  email: string
+}
+export type User = {
+  email: string
+  password: string
+}
